48b0c374d71cde0470122eb83abb7c8496d4d1dc
Oskar <okyjulian994@gmail.com>
2025-11-20 07:42:11 -0500

    "Modificacion vite.config.ts"

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,22 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
     host: true,
+    port: 5173,
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: false,
   },
   resolve: {
     alias: {
       "@": "/src",
     },
   },
+
+  // 🔥 FIX PARA VERCEL (RUTAS SPA)
+  base: "/",
 });
